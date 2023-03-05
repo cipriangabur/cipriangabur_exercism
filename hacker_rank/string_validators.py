408a538c579e34caa2ef4d52b34a6d78bc4df7fe
cipriangabur <ciprian.gabur@gmail.com>
2023-03-05 14:02:17 +0200

    Removed hackerrank progress

--- hacker_rank/string_validators.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    s = input()
-    for action in ["isalnum", "isalpha", "isdigit", "islower", "isupper"]:
-        print(eval(f"\"{s}\".{action}()"))
--- hacker_rank/tuples.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-if __name__ == '__main__':
-    n = int(input())
-    int_list = [int(i) for i in input().split()]
-    int_tuple = tuple(int_list)
-    print(int_tuple)
-    print(hash(int_tuple))
